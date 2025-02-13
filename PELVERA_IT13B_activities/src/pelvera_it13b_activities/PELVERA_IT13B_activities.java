6e1554514ae3ba5c750367824f29069888344329
Stephen <stephenbanaag0303@gmail.com>
2025-02-13 10:57:31 +0800

    Added Java Application

---
+++ PELVERA_IT13B_activities/src/pelvera_it13b_activities/PELVERA_IT13B_activities.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pelvera_it13b_activities;
+
+/**
+ *
+ * @author User
+ */
+public class PELVERA_IT13B_activities {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
