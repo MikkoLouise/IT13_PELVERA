b2eb1d485eaaaac1781b5c6b3bbacead4158033a
Mikko Louise Pelvera <louisemikko11@gmail.com>
2025-02-24 12:00:38 +0800

    Added Java File

---
+++ PELVERA_IT13B_activities/Lab_Exercise2/src/lab_exercise2/Lab_Exercise2.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab_exercise2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class Lab_Exercise2 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x, y, z;
+
+        System.out.println("Input the 1st number");
+        x = in.nextInt();
+        System.out.println("Input the 2nd number");
+        y = in.nextInt();
+        System.out.println("Input the 3rd number");
+        z = in.nextInt();
+        int greatest = x;
+
+        if (y > greatest);
+        greatest = z;
+        if (z > greatest){
+
+        }
+        System.out.println("The greatest number is " + greatest);
+       
+    }
+}
